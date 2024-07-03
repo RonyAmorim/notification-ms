23e364207c55ca4748ae2b4721603098dd5e76a1
Rony Amorim dos Reis <ronyamorimdosreis@gmail.com>
2024-07-03 11:43:07 -0300

    Initial commit

---
+++ src/test/java/com/rony/notification_ms/NotificationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rony.notification_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
